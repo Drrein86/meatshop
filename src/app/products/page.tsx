33c9e0d3c6f61995335b1d87dca827b9103d6a98
Drrein86 <elior2280@gmail.com>
2025-01-11 20:24:04 +0200

    Update API URL to work with Vercel

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -32,7 +32,7 @@ const OrderPage = () => {
   const { cart, addToCart } = useCart(); // גישה לפונקציות העגלה
 
   useEffect(() => {
-    fetch("/api/db", {
+    fetch("https://kezez-place.com/api/db", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
