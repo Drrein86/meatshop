f7c10fbb9feb2d28def1c4ec9e44fa7ae16cf247
Drrein86 <elior2280@gmail.com>
2025-01-13 00:33:32 +0200

    Update API URL to work with Vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,7 @@ const LoginPage = () => {
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
+      <p>hhh</p>
       <div className="text-center">
         {!session ? (
           <>
