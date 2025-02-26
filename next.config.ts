7affbad57b563fe03a32bc9ad1aa346508a4c4be
Drrein86 <elior2280@gmail.com>
2025-02-27 01:02:53 +0200

    Update API URL to work with Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['kezez-place.com'], // הוספת הדומיין של התמונות שלך
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*', // כל בקשה ל-`/api/*`
-        destination: 'https://kezez-place.com/api/:path*', // תנותב ל-API
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
